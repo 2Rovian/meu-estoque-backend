75a2087944babaa4a60865d3ed63de8e31ce099a
Ian Gurgel <iangurgel25@hotmail.com>
2025-09-27 18:35:48 -0300

    add constructor no ProductResponseDTO

--- src/main/java/com/meuestoque/meuestoque/dto/ProdutoResponseDTO.java
+++ src/main/java/com/meuestoque/meuestoque/dto/ProdutoResponseDTO.java
@@ -1,4 +1,9 @@
 package com.meuestoque.meuestoque.dto;
 
+import com.meuestoque.meuestoque.entities.ProdutoEntity;
+
 public record ProdutoResponseDTO(Long id, String name, Double price, Integer quantity) {
+    public ProdutoResponseDTO(ProdutoEntity produto){
+        this(produto.getId(), produto.getName(), produto.getPrice(), produto.getQuantity());
+    }
 }
